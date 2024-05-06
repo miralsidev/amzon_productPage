086ba0d638260bdbf0bebc2a3ad1638466f07f10
miralsidev <miralsidev@gmail.com>
2024-05-06 09:19:45 +0530

    add code

---
+++ index.js
@@ -0,0 +1,36 @@
+
+// abc("a");
+
+// setTimeout(function() {
+//     abc("B");
+// }, 2000);
+
+// abc("c");
+// abc("d");
+// abc("e");
+// function abc(message) {
+//     console.log(message);
+// }
+
+// function content(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//   }
+//   async function xyz() {
+//     console.log("-----------");
+//     console.log("a");
+//     await content(2000);
+//     console.log("c");
+//     console.log("d");
+//   }
+// xyz();
+  
+
+// console.log("first");
+
+// setTimeout({
+
+// console.log("second");
+// },2000)
+// setTimeout(){
+//     console.log();
+// }
